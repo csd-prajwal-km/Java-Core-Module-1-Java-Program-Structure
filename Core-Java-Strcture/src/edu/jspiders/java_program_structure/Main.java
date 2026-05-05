df0d0ebadf533a6e38526427b41d9d4134734a0f
csd-gowtham-r <csd.gowtham-r@example.com>
2026-05-05 22:00:42 +0530

    Java Program Structure: Source and Byte Files

--- Core-Java-Strcture/src/edu/jspiders/java_program_structure/Main.java
+++ Core-Java-Strcture/src/edu/jspiders/java_program_structure/Main.java
@@ -1,7 +1,10 @@
 package edu.jspiders.java_program_structure;
 
 public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hi");
+		System.out.println("Hello");
+		
 	}
 }
